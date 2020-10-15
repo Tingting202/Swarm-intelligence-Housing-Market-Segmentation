4b6b425cb65bd8807fce1e75a9f79b6175bb9341
Tingting202 <72920086+Tingting202@users.noreply.github.com>
2020-10-15 19:55:03 +0800

    Update PCA.R

--- PCA.R
+++ PCA.R
@@ -6,3 +6,17 @@ library("NbClust") # determining the optimal number of clusters in the data se
 library("fpc") # computing clustering validation statistics
 
 ------PCA to reduce data dimension------
+
+pcamodel <- prcomp(pcadata, scale = TRUE) #pcadata is the data you want conduct PCA
+summary(pcamodel)
+
+plot(pcamodel, main = "", col="green")
+
+pcamodel$x[,1:15]
+
+#construct a dataframe that contains 15 principle components with cumulative proportion of variance equal to 70%
+df_pcscores <- pcamodel$x[,1:15]
+dim(df_pcscores)
+
+eig.val <- get_eigenvalue(pcamodel)
+eig.val
