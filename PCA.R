92eabdfb00a2aa4205c915114c896cddc7a3166d
Tingting202 <72920086+Tingting202@users.noreply.github.com>
2020-10-15 19:49:27 +0800

    Update and rename PCA.rmd to PCA.R

---
+++ PCA.R
@@ -0,0 +1,8 @@
+#library packages
+
+library("cluster") 
+library("factoextra") # data visualization
+library("NbClust") # determining the optimal number of clusters in the data se
+library("fpc") # computing clustering validation statistics
+
+#2.2.1.PCA to reduce data dimension
